ce440f0fd7d40211595dc0bbc06ecf54f485ed8a
Fletcher Nichol <fnichol@nichol.ca>
2022-01-09 16:30:35 -0700

    chore: update i3 config to be ported from Regolith Linux
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ home/.config/i3/vendor/lib/libsh.minimal-minified.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env sh
+# shellcheck disable=SC3043
+
+# BEGIN: libsh.sh
+
+#
+# Copyright 2019 Fletcher Nichol and/or applicable contributors.
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# http://www.apache.org/licenses/LICENSE-2.0> or the MIT license (see
+# <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your option. This
+# file may not be copied, modified, or distributed except according to those
+# terms.
+#
+# libsh.sh
+# --------
+# project: https://github.com/fnichol/libsh
+# author: Fletcher Nichol <fnichol@nichol.ca>
+# version: 0.10.1
+# distribution: libsh.minimal-minified.sh
+# commit-hash: 46134771903ba66967666ca455f73ffc10dd0a03
+# commit-date: 2021-05-08
+# artifact: https://github.com/fnichol/libsh/releases/download/v0.10.1/libsh.minimal.sh
+# source: https://github.com/fnichol/libsh/tree/v0.10.1
+# archive: https://github.com/fnichol/libsh/archive/v0.10.1.tar.gz
+#
+if [ -n "${KSH_VERSION:-}" ]; then
+  eval "local() { return 0; }"
+fi
+check_cmd() {
+  if ! command -v "$1" >/dev/null 2>&1; then
+    return 1
+  fi
+}
+die() {
+  case "${TERM:-}" in
+    *term | alacritty | rxvt | screen | screen-* | tmux | tmux-* | xterm-*)
+      printf -- "\n\033[1;31;40mxxx \033[1;37;40m%s\033[0m\n\n" "$1" >&2
+      ;;
+    *)
+      printf -- "\nxxx %s\n\n" "$1" >&2
+      ;;
+  esac
+  exit 1
+}
+indent() {
+  local _ecfile _ec _orig_flags
+  need_cmd cat
+  need_cmd rm
+  need_cmd sed
+  _ecfile="$(mktemp_file)"
+  _orig_flags="$-"
+  set +e
+  {
+    "$@" 2>&1
+    echo "$?" >"$_ecfile"
+  } | sed 's/^/       /'
+  set "-$(echo "$_orig_flags" | sed s/s//g)"
+  _ec="$(cat "$_ecfile")"
+  rm -f "$_ecfile"
+  unset _ecfile _orig_flags
+  return "${_ec:-5}"
+}
+info() {
+  case "${TERM:-}" in
+    *term | alacritty | rxvt | screen | screen-* | tmux | tmux-* | xterm-*)
+      printf -- "\033[1;36;40m  - \033[1;37;40m%s\033[0m\n" "$1"
+      ;;
+    *)
+      printf -- "  - %s\n" "$1"
+      ;;
+  esac
+}
+# shellcheck disable=SC2120
+mktemp_file() {
+  need_cmd mktemp
+  if [ -n "${1:-}" ]; then
+    mktemp "$1/tmp.XXXXXX"
+  else
+    mktemp 2>/dev/null || mktemp -t tmp
+  fi
+}
+need_cmd() {
+  if ! check_cmd "$1"; then
+    die "Required command '$1' not found on PATH"
+  fi
+}
+section() {
+  case "${TERM:-}" in
+    *term | alacritty | rxvt | screen | screen-* | tmux | tmux-* | xterm-*)
+      printf -- "\033[1;36;40m--- \033[1;37;40m%s\033[0m\n" "$1"
+      ;;
+    *)
+      printf -- "--- %s\n" "$1"
+      ;;
+  esac
+}
+
+# END: libsh.sh
